7cf282a71821a064a92e4780e1f7d3017375a7b5
qin <877795526@qq.com>
2023-09-24 23:43:23 +0800

    增加influxdb组件

---
+++ QinSoft.Core.Data/InfuxDB/IInfluxManager.cs
@@ -0,0 +1,31 @@
+﻿using QinSoft.Core.Data.MongoDB.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QinSoft.Core.Data.Influx
+{
+    public interface IInfluxManager : IDisposable
+    {
+        /// <summary>
+        /// 获取Influx客户端
+        /// </summary>
+        IInflux GetInflux();
+
+        /// <summary>
+        /// 获取Influx客户端
+        /// </summary>
+        Task<IInflux> GetInfluxAsync();
+
+        /// <summary>
+        /// 获取Influx客户端
+        /// </summary>
+        IInflux GetInflux(string name);
+
+        /// <summary>
+        /// 获取Influx客户端
+        /// </summary>
+        Task<IInflux> GetInfluxAsync(string name);
+    }
+}
---
+++ QinSoft.Core.Data/InfuxDB/InfluxManagerOptions.cs
@@ -0,0 +1,23 @@
+﻿using QinSoft.Core.Configure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QinSoft.Core.Data.Influx
+{
+    /// <summary>
+    /// Influx管理选项
+    /// </summary>
+    public class InfluxManagerOptions
+    {
+        /// <summary>
+        /// 配置名称，默认"InfluxManagerConfig"
+        /// </summary>
+        public string ConfigName { get; set; } = "InfluxManagerConfig";
+
+        /// <summary>
+        /// 配置格式，默认"ConfigFormat.XML"
+        /// </summary>
+        public ConfigFormat ConfigFormat { get; set; } = ConfigFormat.XML;
+    }
+}
