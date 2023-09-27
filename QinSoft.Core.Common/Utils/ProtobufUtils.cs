331c62e5d5d9f5cb5fb7cc9e29b5dc0ef1da6771
qinhouping <877795526@qq.com>
2023-09-28 00:08:18 +0800

    优化protobuf工具

--- QinSoft.Core.Common/Utils/ProtobufUtils.cs
+++ QinSoft.Core.Common/Utils/ProtobufUtils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Google.Protobuf;
+using log4net;
 
 /// <summary>
 /// protobuf工具类
@@ -11,8 +12,18 @@ namespace QinSoft.Core.Common.Utils
 {
     public static class ProtobufUtils
     {
+        /// <summary>
+        /// Protobuf默认日志
+        /// </summary>
+        public static ILog DefaultLog { get; set; } = LogManager.GetLogger(typeof(ProtobufUtils));
+
         public static byte[] ToProtobuf<T>(this T obj) where T : IMessage
         {
+            if (null == obj)
+            {
+                DefaultLog?.Debug("to protobuf: null");
+                return null;
+            }
             using (MemoryStream ms = new MemoryStream()) {
                 using (CodedOutputStream stream = new CodedOutputStream(ms))
                 {
@@ -24,6 +35,12 @@ namespace QinSoft.Core.Common.Utils
 
         public static T FromProtobuf<T>(byte[] bytes) where T : IMessage,new()
         {
+            if (null == bytes)
+            {
+                DefaultLog?.Debug("from protobuf: null");
+                return default;
+            }
+            
             var message = new T();
             using (var stream = new CodedInputStream(bytes))
             {
